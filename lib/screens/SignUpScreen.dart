4941ab88dafadc6b595144825b89bf0038054665
Tajdar Raza <tajdar.tr@gmail.com>
2023-04-09 04:29:37 +0530

    update

--- lib/screens/SignUpScreen.dart
+++ lib/screens/SignUpScreen.dart
@@ -1,20 +1,25 @@
 import 'package:flutter/material.dart';
 
-class SignUpScreen extends StatefulWidget{
+class SignUpScreen extends StatefulWidget {
   const SignUpScreen({super.key, required this.title});
 
   final String title;
   @override
   State<SignUpScreen> createState() => _SignUpScreen();
 }
+
 class _SignUpScreen extends State<SignUpScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: Text("Helllo Signup"),
+      body: Padding(
+        padding: const EdgeInsets.all(10),
+        child: ListView(
+          children: const <Widget>[
+            Text("Hello Signup")
+          ],
+        ),
       ),
     );
   }
-
 }
